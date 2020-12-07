12f9b9a2483f2ce39273e2128ffe069dff33261b
M15 <ismaelmelgar1999@gmail.com>
2020-12-07 11:44:16 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/ismael/tarea4_ismael/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ismael.tarea4_ismael;
+
+/**
+ *
+ * @author ismae
+ */
+public class Persona {
+    int edad;
+    String colorPiel;
+    double altura;
+    //asdafaf
+}
